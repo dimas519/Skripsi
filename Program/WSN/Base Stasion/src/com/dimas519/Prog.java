cda6fbb89285eb7bc135a21d40697a0accb923a4
Dimas Kurniawan <kurniawandimas519@yahoo.com>
2023-01-28 01:19:33 +0700

    add wsn initial code

---
+++ Program/WSN/Base Stasion/src/com/dimas519/Prog.java
@@ -0,0 +1,16 @@
+package com.dimas519;
+
+import com.virtenio.misc.PropertyHelper;
+
+public class Prog {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.flush();
+
+
+
+
+
+
+	}
+}
