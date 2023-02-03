b6ee50e647e341217714c67eaa59bc6d9e228aef
dimas <kurniawandimas519@yahoo.com>
2023-02-04 02:47:12 +0700

    repair docker

--- Program/API/createDocker.bat
+++ Program/API/createDocker.bat
@@ -1,16 +1,12 @@
 docker network create skripsi_network
 
-docker container create --name api-db-1 --network skripsi_network -p  9926:3306 mysql:8.0
+docker-compose -f .docker/docker-compose.yml up -d
 
-docker container create --name api-phpmyadmin-1 --network skripsi_network -p  9927:80 phpmyadmin/phpmyadmin:5.1.3
+docker network connect skripsi_network skripsi-db
 
+docker network connect skripsi_network skripsi-phpmyadmin
 
-
-docker container rm apimain
-
-docker image rm main-api:1.0.0
-
-docker build --tag main-api:1.0.0 . -f .docker/Dockerfile 
+docker build --tag main-api:1.0.0 . -f .docker/Dockerfile
 
 docker container create --name apimain --network skripsi_network -p  5000:5000 main-api:1.0.0
 
